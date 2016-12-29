cabc2e64c96d05a36e90e16c24ca50afd6c6a92e
RiteshChandra05 <kesha.riteshchandragupta@gmail.com>
2016-12-28 23:20:06 -0600

    exception handling

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿//nullables types and anonymous types, var
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,39 +8,23 @@ namespace practise
 {
     class Program
     {
-        public int add()
-        {
-            var a = 5;
-            int b = 5;
-
-            return a + b;
-        }
 
         static void Main(string[] args)
         {
+            int a = 5;
+            int b = 0;
+            double c;
 
 
-            int? num1 = null;
-            int? num2 = 22;
-            double? num3 = new double?();
-            Console.WriteLine("." + num1 + " " + num2 + " " + num3 + ".");// prints blank space at num1 and num3
-
-
-            var any = new
+            try
             {
-                name = "first",
-                rollno = 5,
-                present = true
-            };
-
-            Console.WriteLine(any.name);
-            Console.WriteLine(any.rollno);
-            Console.WriteLine(any.present);
-
-            Program a1 = new Program();
-            Console.WriteLine("a+b=" + a1.add());
-
-
+                c = a / b;
+                Console.WriteLine(c);
+            }
+            catch
+            {
+                Console.WriteLine("no operation");
+            }
         }
     }
 }
